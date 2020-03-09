fec3107e2e5f21a377b289b9d8752134f4ba08e9
Muhammad Junaid Hassan <muhammadjunaidhassan1@gmail.com>
2020-03-10 01:18:01 +0500

    server creation

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import * as Express from 'express';
+import  { buildSchema, Resolver, Query } from 'type-graphql';
+
+@Resolver()
+
+class RecipeResolver{
+    
+    @Query(() => String)
+    async hello(){
+        return "hello world";
+    }
+}
+
+const main = async () =>{
+
+    const schema = await buildSchema({
+        resolvers: [RecipeResolver]
+    });
+
+    const apolloServer = new ApolloServer({
+        schema
+    });
+    
+    const app = Express();
+
+    apolloServer.applyMiddleware({app});
+     
+    app.listen(4000, () =>{
+        console.log('server started on localhost:4000');
+    });
+}
+
+main();
