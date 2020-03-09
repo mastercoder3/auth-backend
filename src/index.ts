b51437d726ef543197f57ab3f9ce3153c09aeca0
Muhammad Junaid Hassan <muhammadjunaidhassan1@gmail.com>
2020-03-10 01:57:39 +0500

    user registeration created

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import * as Express from 'express';
-import  { buildSchema, Resolver, Query } from 'type-graphql';
-
-@Resolver()
-
-class RecipeResolver{
-    
-    @Query(() => String)
-    async hello(){
-        return "hello world";
-    }
-}
+import  { buildSchema} from 'type-graphql';
+import { createConnection } from 'typeorm';
+import { RegisterResolver } from './modules/user/Register';
 
 const main = async () =>{
 
+    await createConnection();
+
     const schema = await buildSchema({
-        resolvers: [RecipeResolver]
+        resolvers: [RegisterResolver]
     });
 
     const apolloServer = new ApolloServer({
